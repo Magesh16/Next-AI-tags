04aaf1c3f45b930e2ec488e15ebd84a9f0f2f1c8
Magesh16 <mageshsk16@gmail.com>
2023-06-24 23:36:20 +0530

    Navbar and components constructed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import '@styles/globals.css';
+import React from "react";
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   titlle: "Next-AI",
-  description:"Discover the AI tags and share the ideas"
-}
+  description: "Discover the AI tags and share the ideas",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
             {children}
-        </main>
+          </main>
+        </Provider>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Feed from '@components/Feed'
 
 const page = () => {
   return (
-    <div>HOME</div>
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
+            Explore & Blast
+            <br className='max-md:hidden' />
+        </h1>
+        <h3 className='violet_gradient main_text text-center'>Next-AI Powered </h3>
+        <p className='desc text-center'> 
+            This AI will provide you the prompt of searched tags and this is next generative AI tool
+            for all techies.
+        </p>
+        <Feed/>
+    </section>
   )
 }
 
