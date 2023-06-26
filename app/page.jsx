45a9ca75ed7e3e4de02eff640ddf92d4e898468b
Magesh16 <mageshsk16@gmail.com>
2023-06-26 16:51:13 +0530

    last changes

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,13 @@ const page = () => {
   return (
     <section className='w-full flex-center flex-col'>
         <h1 className='head_text text-center'>
-            Explore & Blast
+            Explore & Learn
             <br className='max-md:hidden' />
         </h1>
-        <h3 className='violet_gradient main_text text-center'>Next-AI Powered </h3>
+        <h3 className='violet_gradient main_text text-center'>Next-AI Powered</h3>
         <p className='desc text-center'> 
-            This AI will provide you the prompt of searched tags and this is next generative AI tool
-            for all techies.
+            This app will be very informative to gather new technologies and 
+            ideas. These prompts can be used later to brief with some AI tool.
         </p>
         <Feed/>
     </section>
